5b435a656e4fb378ed839f7cdac3c0db8c204f4a
Selesito <selesitomorozov@gmail.com>
2021-04-21 10:51:48 +0400

    3. Прерывание нити [#522819]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        int count = 0;
+        char[] process = {'\\', '|', '/', '-'};
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[count++]);
+            if (count == 4) {
+                count = 0;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1);
+        progress.interrupt();
+    }
+}
