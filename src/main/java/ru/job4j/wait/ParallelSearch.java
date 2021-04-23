0b01039b69c691e63f9a04b5ad6bea17485c8561
Selesito <selesitomorozov@gmail.com>
2021-04-23 21:58:59 +0400

    2. Обеспечить остановку потребителя. [#522809]

---
+++ src/main/java/ru/job4j/wait/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.wait;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
+
