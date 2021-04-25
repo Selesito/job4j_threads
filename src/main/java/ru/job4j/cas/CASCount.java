cb279c91df0a7aa9a3c76182c42727da59bd425e
Selesito <selesitomorozov@gmail.com>
2021-04-25 13:03:19 +0400

    0. CAS - операции [#522839]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,23 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int curCount;
+        int newCount;
+        do {
+            curCount = count.get();
+            newCount = curCount++;
+        } while (!count.compareAndSet(curCount, newCount));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
