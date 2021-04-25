da040579918117882771b1d93fa9002b2bd1a9ac
Selesito <selesitomorozov@gmail.com>
2021-04-25 12:19:44 +0400

    3. Junit тест для блокирующей очереди. [#522810

--- src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -2,22 +2,21 @@ package ru.job4j.wait;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
-
     @Test
-    public void whenOfferAndPoll() throws InterruptedException {
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
         final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-        final List<Integer> rsl = new ArrayList<>();
-        Thread offer = new Thread(
+        Thread producer = new Thread(
                 () -> {
-                    for (int i = 0; i < 8; i++) {
+                    for (int i = 0; i < 5; i++) {
                         try {
                             queue.offer(i);
                         } catch (InterruptedException e) {
@@ -26,11 +25,12 @@ public class SimpleBlockingQueueTest {
                     }
                 }
         );
-        Thread poll = new Thread(
+        producer.start();
+        Thread consumer = new Thread(
                 () -> {
                     while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
                         try {
-                            rsl.add(queue.poll());
+                            buffer.add(queue.poll());
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                             Thread.currentThread().interrupt();
@@ -38,11 +38,10 @@ public class SimpleBlockingQueueTest {
                     }
                 }
         );
-        offer.start();
-        poll.start();
-        offer.join();
-        poll.interrupt();
-        poll.join();
-        assertThat(rsl, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7)));
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 }
