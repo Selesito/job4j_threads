36ef484d8b87b6d8f57947f6a008dcfdfa751a53
Selesito <selesitomorozov@gmail.com>
2021-05-04 10:34:53 +0400

    4. CompletableFuture [#522806]

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -1,5 +1,6 @@
 package ru.job4j.pool;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -27,6 +28,32 @@ public class RolColSum {
 
         public void setColSum(int colSum) {
             this.colSum = colSum;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Sums sums = (Sums) o;
+            return rowSum == sums.rowSum
+                    && colSum == sums.colSum;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(rowSum, colSum);
+        }
+
+        @Override
+        public String toString() {
+            return "Sums{"
+                    + "rowSum=" + rowSum
+                    + ", colSum=" + colSum
+                    + '}';
         }
     }
 
