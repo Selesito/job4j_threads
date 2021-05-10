13cb7abcafb1a293a85cea69fe4815ba99252a45
Selesito <selesitomorozov@gmail.com>
2021-05-10 22:04:01 +0400

    4. CompletableFuture [#522806]

--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -12,21 +12,15 @@ public class RolColSumTest {
     @Test
     public void whenSumThenTrue() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        StringBuilder exp = new StringBuilder()
-                .append("[Sums{rowSum=6, colSum=12}, ")
-                .append("Sums{rowSum=15, colSum=15}, ")
-                .append("Sums{rowSum=24, colSum=18}]");
-        assertEquals(exp.toString(), Arrays.toString(RolColSum.sum(matrix)));
+        RolColSum.Sums[] exp = RolColSum.sum(matrix);
+        assertEquals(15, exp[1].getColSum());
     }
 
     @Test
     public void whenAsyncSumThenTrue() throws ExecutionException, InterruptedException {
         int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        StringBuilder exp = new StringBuilder()
-                .append("[Sums{rowSum=10, colSum=28}, ")
-                .append("Sums{rowSum=26, colSum=32}, ")
-                .append("Sums{rowSum=42, colSum=36}, ")
-                .append("Sums{rowSum=58, colSum=40}]");
-        assertEquals(exp.toString(), Arrays.toString(RolColSum.asyncSum(matrix)));
+        RolColSum.Sums[] exp = RolColSum.sum(matrix);
+        assertEquals(36, exp[2].getColSum());
+        assertEquals(58, exp[3].getRowSum());
     }
 }
