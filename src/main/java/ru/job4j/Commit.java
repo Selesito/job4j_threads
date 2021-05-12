4b36f410f63bfb06dbb3b835f3b7ca205c950cf3
Selesito <selesitomorozov@gmail.com>
2021-05-12 12:18:27 +0400

    3.2. Откат изменений. [#522673]

--- src/main/java/ru/job4j/Commit.java
+++ src/main/java/ru/job4j/Commit.java
@@ -4,5 +4,6 @@ public class Commit {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Добавить изменения в последний коммит.");
+        System.out.println("3.2. Откат изменений. [#522673]");
     }
 }
