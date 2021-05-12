5469f1a2db98fd72c2603de919f7279c7c501898
Selesito <selesitomorozov@gmail.com>
2021-05-12 11:42:43 +0400

    3.1. Добавить изменения в последний коммит. [#522672]

---
+++ src/main/java/ru/job4j/Commit.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+
+public class Commit {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Добавить изменения в последний коммит.");
+    }
+}
