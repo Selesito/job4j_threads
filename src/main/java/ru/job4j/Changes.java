e4a156af769d254bb8965d4594b4d52c6dc41b4d
Selesito <selesitomorozov@gmail.com>
2021-05-14 09:34:28 +0400

    3.4. Временное хранение. git stash. [#522674]

--- src/main/java/ru/job4j/Changes.java
+++ src/main/java/ru/job4j/Changes.java
@@ -3,5 +3,6 @@ package ru.job4j;
 public class Changes {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Перенесен во Временное хранение");
     }
 }
